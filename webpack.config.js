224c41c4b81d16f6ade23b7f30ff1a734044835b
Tracy Yu <tracyyu93@g.ucla.edu>
2019-04-02 23:03:24 -0700

    Install file structure for navbar

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+const combinedLoaders = require('webpack-combine-loaders');
+
+module.exports = {
+  entry: __dirname + '/client/index.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        loader: combinedLoaders([
+         {
+           loader: 'style-loader'
+         }, {
+           loader: 'css-loader',
+           query: {
+             modules: true,
+             localIdentName: '[name]__[loader]__[hash:base64:5]'
+           }
+         }, {
+           loader : 'sass-loader'
+         }
+        ])
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|otf)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=1000',
+        options: {
+          fallback: 'file-loader'
+        }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
